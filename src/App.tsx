a1039dc24039cc2d17ef4a936d892808ee8600ac
alstn <tkops365@gmail.com>
2021-12-02 21:53:25 +0900

    분할링크와 버튼 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import { Grid } from "@mui/material";
-import { Box } from "@mui/system";
+import BottomAppBar from "./components/BottomAppBar";
 
 const App = (): JSX.Element => {
   return (
@@ -11,37 +10,7 @@ const App = (): JSX.Element => {
         <Toolbar></Toolbar>
       </AppBar>
       <Toolbar />
-      <AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
-        <Toolbar>
-          <Grid container>
-            <Grid item xs>
-              <Box display="flex" justifyContent="center">
-                홈
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box display="flex" justifyContent="center">
-                동네생활
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box display="flex" justifyContent="center">
-                내 근처
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box display="flex" justifyContent="center">
-                나의 당근
-              </Box>
-            </Grid>
-            <Grid item xs>
-              <Box display="flex" justifyContent="center">
-                채팅
-              </Box>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+      <BottomAppBar />
     </main>
   );
 };
